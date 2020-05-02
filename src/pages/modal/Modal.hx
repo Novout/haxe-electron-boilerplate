769419f77180a1d2c650ae1e12288b442b171467
Novout <novout@hotmail.com>
2020-05-02 18:45:24 -0300

    feat: close event modal

--- src/pages/modal/Modal.hx
+++ src/pages/modal/Modal.hx
@@ -3,14 +3,33 @@ package pages.modal;
 import js.Browser.console;
 import js.Browser.window;
 import js.Browser.document;
+import electron.renderer.Remote;
 
 class Modal {
 
     static function main() {
         window.onload = function() {
+            var container = document.querySelector('#container');
+
+            container.addEventListener('click', function() {
+                console.log('Event in Modal');
+            });
+
             var title = document.querySelector('#title');
             title.innerHTML = "Hello World";
             title.style.color = "black";
+
+            var button = document.querySelector('#close');
+            button.style.padding = "10px";
+            button.style.marginTop = "50px";
+            button.style.backgroundColor = "#303030";
+            button.style.border = "none";
+            button.style.color = "white";
+
+            button.addEventListener('click', function() {
+                var win = Remote.getCurrentWindow();
+                win.close();
+            });
         }
     }
 }
