671920cf437657491f5b85a4b877a40f716910fa
Novout <novout@hotmail.com>
2020-05-02 13:34:08 -0300

    chore(structure): initiat haxe-electron

---
+++ src/electron/Main.hx
@@ -0,0 +1,44 @@
+
+package electron;
+
+import js.Node;
+import js.Node.__dirname;
+import electron.main.App;
+import electron.main.BrowserWindow;
+
+class Main {
+
+	static public function main() {
+
+		electron.main.App.on(ready, function(e) {
+
+			var mainWindow = new BrowserWindow({
+				width: 1080, 
+				height: 720,
+				frame: true,
+				webPreferences: {
+					nodeIntegration: true
+				}
+			});
+
+			mainWindow.on(closed, function() {
+				mainWindow = null;
+			});
+
+			mainWindow.setMenuBarVisibility(false);
+
+			mainWindow.setResizable(false);
+			
+			mainWindow.loadFile('./src/pages/app.html');
+
+			// win.webContents.openDevTools();
+
+			// var tray = new electron.main.Tray( '${__dirname}/icon-192.png' );
+		});
+
+		electron.main.App.on( window_all_closed, function(e) {
+			if( Node.process.platform != 'darwin' ) electron.main.App.quit();
+		});
+	}
+
+}
---
+++ src/pages/App.hx
@@ -0,0 +1,18 @@
+package pages;
+
+import js.Browser.console;
+import js.Browser.document;
+import js.Browser.window;
+import js.Node.process;
+import js.node.ChildProcess;
+
+class App {
+
+    static function main() {
+
+        window.onload = function() {
+            document.getElementById( 'logo-haxe' ).style.opacity = '1';
+        }
+    }
+
+}
