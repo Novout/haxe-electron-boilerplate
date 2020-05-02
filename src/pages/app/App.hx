7bb050ac0f3409eb077e8ec7315a227b82c625ca
Novout <novout@hotmail.com>
2020-05-02 15:51:17 -0300

    feat: landing page

--- src/pages/app/App.hx
+++ src/pages/app/App.hx
@@ -5,14 +5,26 @@ import js.Browser.document;
 import js.Browser.window;
 import js.Node.process;
 import js.node.ChildProcess;
+import electron.renderer.Remote;
 
 class App {
+
+    static inline function setText( id : String, text : String ) {
+        document.getElementById( id ).textContent = text;
+    }
 
     static function main() {
 
         window.onload = function() {
-            document.getElementById( 'logo-haxe' ).style.opacity = '1';
+
+            setText( 'system-version', process.platform +' '+ process.arch );
+            setText( 'node-version', 'node '+process.version );
+            setText( 'electron-version', 'electron '+process.versions['electron'] );
+
+            ChildProcess.spawn( 'haxe', ['-version'] ).stdout.on( 'data', function(buf) {
+				var version = buf.toString();
+                setText( 'haxe-version', 'haxe $version' );
+            });
         }
     }
-
 }
